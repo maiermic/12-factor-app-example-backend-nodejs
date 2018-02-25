f4f720138097c36134c338cfd46c4e189bd2cdb0
Michael Maier <maier1michael@gmail.com>
2018-02-25 16:22:08 +0100

    authenticate using docker secrets credentials

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const secrets = require('docker-secrets');
 const {MongoClient} = require('mongodb');
 
 console.log(`connect to database ${process.env.MONGO_URI}`);
-MongoClient.connect(process.env.MONGO_URI, function (err, db) {
-    if (err) {
-        console.log('Cannot connect to MongoDB!', err);
-    } else {
-        console.log('Connected to MongoDB!');
-        console.log('Close database connection');
-        db.close();
+console.log('secrets', JSON.stringify(secrets, null, 4))
+MongoClient.connect(
+    process.env.MONGO_URI,
+    {
+        auth: {
+            user: secrets.mongo_user,
+            password: secrets.mongo_password,
+        },
+        authSource: 'admin'
+    },
+    function (err, client) {
+        if (err) {
+            console.log('Cannot connect to MongoDB!', err);
+        } else {
+            console.log('Connected to MongoDB!');
+            console.log(`database name ${client.db('admin').databaseName}`);
+            console.log('Close database connection');
+            client.close();
+        }
+        process.exit();
     }
-    process.exit();
-});
+);
