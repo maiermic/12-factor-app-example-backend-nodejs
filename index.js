fbac4048be6d0b5ff07aadd20a3acd403ddd954b
Michael Maier <maier1michael@gmail.com>
2018-02-25 23:38:47 +0100

    add routes to post and get images

--- index.js
+++ index.js
@@ -16,10 +16,43 @@ mongoose.connect(
     function onConnectionSuccess() {
         console.log('Connected to MongoDB!');
         console.log(`database name ${mongoose.connection.db.databaseName}`);
+        const Image = new mongoose.Schema({
+            title: String,
+            url: String,
+        });
+        const ImageModel = mongoose.model('Image', Image);
         const express = require('express');
         const app = express();
-        app.get('/', (req, res) => {
-            res.send('hello world');
+        const bodyParser = require('body-parser');
+        const jsonParser = bodyParser.json();
+        app.post('/images/', jsonParser, (req, res) => {
+            console.log('req.body', req.body);
+            if (!req.body || !req.body.title || !req.body.url) {
+                return res.sendStatus(400);
+            }
+            new ImageModel({
+                title: req.body.title,
+                url: req.body.url,
+            }).save().then(
+                function () {
+                    res.send('Image saved');
+                },
+                function (err) {
+                    console.log('Could not save image', err);
+                    res.sendStatus(500);
+                }
+            )
+        });
+        app.get('/images/', jsonParser, (req, res) => {
+            ImageModel.find().exec().then(
+                function (images) {
+                    res.json(images.map(({title, url}) => ({title, url})));
+                },
+                function (err) {
+                    console.log('Could not get images', err);
+                    res.sendStatus(500);
+                }
+            )
         });
         app.listen(process.env.PORT, () => {
             console.log(`Server listening on port ${process.env.PORT}!`);
