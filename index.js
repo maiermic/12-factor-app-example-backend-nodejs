a539c87afe30561b0b8e2552eb427ecc4a15c4a6
Michael Maier <maier1michael@gmail.com>
2018-02-25 22:33:45 +0100

    use mongoose instead of mongodb

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const secrets = require('docker-secrets');
-const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
 
 console.log(`connect to database ${process.env.MONGO_URI}`);
 console.log('secrets', JSON.stringify(secrets, null, 4))
-MongoClient.connect(
+mongoose.connect(
     process.env.MONGO_URI,
     {
         auth: {
@@ -11,16 +11,17 @@ MongoClient.connect(
             password: secrets.mongo_password,
         },
         authSource: 'admin'
+    }
+).then(
+    function onConnectionSuccess() {
+        console.log('Connected to MongoDB!');
+        console.log(`database name ${mongoose.connection.db.databaseName}`);
+        console.log('Close database connection');
+        mongoose.connection.close();
+        process.exit();
     },
-    function (err, client) {
-        if (err) {
-            console.log('Cannot connect to MongoDB!', err);
-        } else {
-            console.log('Connected to MongoDB!');
-            console.log(`database name ${client.db('admin').databaseName}`);
-            console.log('Close database connection');
-            client.close();
-        }
+    function onConnectionFailure(err) {
+        console.log('Cannot connect to MongoDB!', err);
         process.exit();
     }
 );
