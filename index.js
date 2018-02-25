ec0d7115cfadcd5dc836fa0492012ff7f05c4212
Michael Maier <maier1michael@gmail.com>
2018-02-25 12:57:46 +0100

    log to which database the app connects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {MongoClient} = require('mongodb');
 
+console.log(`connect to database ${process.env.MONGO_URI}`);
 MongoClient.connect(process.env.MONGO_URI, function (err, db) {
     if (err) {
         console.log('Cannot connect to MongoDB!', err);
