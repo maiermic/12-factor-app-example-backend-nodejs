01032b18c73364b80d90f0c8bbdc23124edbe1c7
Michael Maier <maier1michael@gmail.com>
2018-02-25 13:18:16 +0100

    close database connection

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ MongoClient.connect(process.env.MONGO_URI, function (err, db) {
         console.log('Cannot connect to MongoDB!', err);
     } else {
         console.log('Connected to MongoDB!');
+        console.log('Close database connection');
+        db.close();
     }
     process.exit();
 });
