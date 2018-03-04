c7a9f73626768e6a50a2c17f13094a1ec579b0f5
Michael Maier <maier1michael@gmail.com>
2018-03-04 23:43:46 +0100

    upload file

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const secrets = require('docker-secrets');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const request = require('request');
+const tempfile = require('tempfile');
 
 console.log(`connect to database ${process.env.MONGO_URI}`);
 console.log('secrets', JSON.stringify(secrets, null, 4));
@@ -18,7 +23,7 @@ mongoose.connect(
         console.log(`database name ${mongoose.connection.db.databaseName}`);
         const Image = new mongoose.Schema({
             title: String,
-            url: String,
+            urlPath: String,
         });
         const ImageModel = mongoose.model('Image', Image);
         const express = require('express');
@@ -27,19 +32,17 @@ mongoose.connect(
         const jsonParser = bodyParser.json();
         app.post('/images/', jsonParser, async (req, res) => {
             console.log('req.body', req.body);
-            if (!req.body || !req.body.title || !req.body.url) {
+            if (!req.body || !req.body.title) {
                 return res.sendStatus(400);
             }
             try {
                 const image = await new ImageModel({
                     title: req.body.title,
-                    url: req.body.url,
                 }).save();
-                console.log('Image saved', image);
+                console.log('Image added', image);
                 res.json({
                     id: image._id,
                     title: image.title,
-                    url: image.url,
                 });
             } catch (err) {
                 console.log('Could not save image', err);
@@ -52,6 +55,42 @@ mongoose.connect(
                 res.json(images.map(({_id, title, url}) => ({id: _id, title, url})));
             } catch (err) {
                 console.log('Could not get images', err);
+                res.sendStatus(500);
+            }
+        });
+        const fileUpload = require('express-fileupload');
+        app.put('/images/:id/upload', fileUpload(), async (req, res) => {
+            if (!req.files) {
+                return res.status(400).send('No files were uploaded.');
+            }
+            const files = Object.values(req.files);
+            if (files.length !== 1) {
+                return res.status(400).send('Only one file can be uploaded.');
+            }
+            const file = files[0];
+            const fileExt = path.extname(file.name);
+            const id = req.params.id;
+            try {
+                const tempImageFile = tempfile(fileExt);
+                await file.mv(tempImageFile);
+                console.log(`Get upload url of image ${id}`);
+                const image = await ImageModel.findById(id).exec();
+                const urlPath = id + fileExt;
+                const uploadUrl = url.resolve(process.env.BASE_IMAGE_URL, urlPath);
+                console.log(`Upload image ${id} to ${uploadUrl}`);
+                await request.post(uploadUrl, {
+                    formData: {
+                        file: fs.createReadStream(tempImageFile)
+                    }
+                });
+                await image.set({urlPath}).save();
+                res.send({
+                    id: image._id,
+                    title: image.title,
+                    url: uploadUrl,
+                });
+            } catch (err) {
+                console.log(`Error uploading image ${id}`, err);
                 res.sendStatus(500);
             }
         });
